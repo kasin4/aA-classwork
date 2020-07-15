0b50ee4ac58d83c7e0d8bee3a78e612d58267ff2
Greg Kasin <kasin4@gmail.com>
2020-07-14 22:58:05 -0700

    W7_D1

---
+++ W7_D2/music/app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+
+  def create
+    user = User.find_by_credentials(
+    params[:user][:email],
+    params[:user][:password]
+    )
+
+    if user.nil?
+      flash.now[:errors] = ["Incorrect email/password"]
+      render :new
+    else
+      log_in_user!(user)
+      redirect_to user_url(user.id)
+    end
+  end
+
+  def new
+    render :new
+  end
+
+  def destroy
+    logout_user!
+    redirect_to new_session_url
+  end
+
+end
